222b67deb6fa8172baefce9377e5cb9c89e392e2
Dmitry Avseyko <dima.milan5182@gmail.com>
2024-09-17 15:52:51 +0300

    hw_1

---
+++ hw_1.py
@@ -0,0 +1,20 @@
+print('Hello world!')
+
+num = 100
+name = 'Алексей'
+first_number = 1
+text = 'мой текст'
+phone_number = '+375757777777'
+
+name = 'Дима'
+print('Привет, меня зовут' + ' ' + name + '.')
+print(f'Привет, меня зовут {name}.')
+
+name = input()
+surname = input()
+print(f'Привет, меня зовут {name.title()} {surname.title()}.')
+
+a = input()
+b = input()
+result = int(a) + int(b)
+print(result)
